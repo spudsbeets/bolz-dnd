5ad3bab8d5ae9df42fc3d373d3a2fda4e4ebe9e1
Sean Miller <millersean757@gmail.com>
2024-06-11 17:02:08 -0600

    donzo

--- src/components/battlefield.tsx
+++ src/components/battlefield.tsx
@@ -18,7 +18,7 @@ function Battlefield({ battlefield, setBattle }: PropsType) {
 
     let count: number = 0
 
-    function setMovement(el: HTMLDivElement) {
+    function setMovement(el: HTMLDivElement | HTMLImageElement) {
         if (el.innerHTML !== "") {
             setFromId(el.id)
             setFrom(el.innerHTML)
@@ -32,6 +32,9 @@ function Battlefield({ battlefield, setBattle }: PropsType) {
     useEffect(() => {
         if (from === "" || toggle === false) {
             null
+        } else if (toId === "trash") {
+            console.log(toId);
+            (document.getElementById(fromId) as HTMLDivElement).innerHTML = ""
         } else {
             (document.getElementById(fromId) as HTMLDivElement).innerHTML = "";
             (document.getElementById(toId) as HTMLDivElement).innerHTML = from
@@ -45,6 +48,7 @@ function Battlefield({ battlefield, setBattle }: PropsType) {
         block.style.width = "80px"
         block.style.height = "80px"
         block.style.backgroundColor = "gray"
+        block.style.cursor = "pointer"
         block.style.border = "solid black 2px";
         block.onclick = function(){setMovement(this as HTMLDivElement)};
         count++
@@ -60,10 +64,24 @@ function Battlefield({ battlefield, setBattle }: PropsType) {
             );
             count++
         } while (count !== goal)
+        const trash: HTMLImageElement = document.createElement('img');
+        trash.id = "trash"
+        trash.style.width = "160px"
+        trash.style.height = "160px"
+        trash.style.backgroundColor = "none"
+        trash.src = "./src/images/trash.png"
+        trash.alt = "trash"
+        trash.style.position = "fixed"
+        trash.style.right = "0"
+        trash.style.marginTop = "256px"
+        trash.style.cursor = "pointer"
+        trash.onclick = function(){setMovement(this as HTMLImageElement)};
+        (document.getElementById("trash") as HTMLDivElement).appendChild(trash)
         const jake: HTMLImageElement = document.createElement('img');
         jake.style.width = "60px"
         jake.style.height = "60px"
         jake.style.margin = "auto"
+        jake.style.marginTop = "10px"
         jake.src = "./src/images/jake.png"
         jake.alt = "jake";
         (document.getElementById("0-block") as HTMLDivElement).appendChild(jake);
@@ -71,6 +89,7 @@ function Battlefield({ battlefield, setBattle }: PropsType) {
         keith.style.width = "60px"
         keith.style.height = "60px"
         keith.style.margin = "auto"
+        keith.style.marginTop = "10px"
         keith.src = "./src/images/keith.png"
         keith.alt = "keith";
         (document.getElementById("1-block") as HTMLDivElement).appendChild(keith);
@@ -88,6 +107,7 @@ function Battlefield({ battlefield, setBattle }: PropsType) {
                 en.style.width = "60px"
                 en.style.height = "60px"
                 en.style.margin = "auto"
+                en.style.marginTop = "10px"
                 en.src = enemies[enemyIndex].img
                 en.alt = enemies[enemyIndex].name;
                 (document.getElementById(enemies[enemyIndex].appendIndex + "-block") as HTMLDivElement).appendChild(en);
@@ -97,14 +117,15 @@ function Battlefield({ battlefield, setBattle }: PropsType) {
 
     return(
         <>
+        <div id="trash"></div>
         <div id="grid" className="flex flex-row flex-wrap justify-center mx-auto" style={{
             width: String(Number(battlefield.width) * 80) + "px"
             }}>
         </div>
-        <button className="fixed top-0 left-0 text-4xl bg-slate-900 hover:bg-slate-500 border:slate-500 border-8" onClick={() => {
+        <button className="fixed top-0 left-0 text-4xl bg-slate-900 hover:bg-slate-500 border-slate-500 border-8" onClick={() => {
             populateGrid()
             }}>POPULATE</button>
-        <button className="fixed top-0 right-0 text-4xl bg-slate-900 hover:bg-slate-500 border:slate-500 border-8" onClick={() => {
+        <button className="fixed top-0 right-0 text-4xl bg-slate-900 hover:bg-slate-500 border-slate-500 border-8" onClick={() => {
             for (let i = 0; i < images.length; i++) {
                 images[i].remove()
             }
--- src/types/enemies.tsx
+++ src/types/enemies.tsx
@@ -7,92 +7,92 @@ type EnemyObject = {
 const enemies: EnemyObject[] = [
     {
         name: "awakened tree 1",
-        img: './src/images/gnome.png',
+        img: './src/images/awakened-tree.png',
         appendIndex: "2"
     },
     {
         name: "awakened tree 2",
-        img: './src/images/gnome.png',
+        img: './src/images/awakened-tree.png',
         appendIndex: "3"
     },
     {
         name: "chipmunk 1",
-        img: './src/images/fairy.png',
+        img: './src/images/chipmunk.png',
         appendIndex: "4"
     },
     {
         name: "chipmunk 2",
-        img: './src/images/fairy.png',
+        img: './src/images/chipmunk.png',
         appendIndex: "5"
     },
     {
         name: "chipmunk 3",
-        img: './src/images/fairy.png',
+        img: './src/images/chipmunk.png',
         appendIndex: "6"
     },
     {
         name: "chipmunk 4",
-        img: './src/images/fairy.png',
+        img: './src/images/chipmunk.png',
         appendIndex: "7"
     },
     {
         name: "undead dragon",
-        img: './src/images/tiefling.png',
+        img: './src/images/undead-dragon.png',
         appendIndex: "8"
     },
     {
         name: "green hag",
-        img: './src/images/fairy.png',
+        img: './src/images/hag.png',
         appendIndex: "9"
     },
     {
         name: "tin man 1",
-        img: './src/images/fairy.png',
+        img: './src/images/tin-man.png',
         appendIndex: "10"
     },
     {
         name: "tin man 2",
-        img: './src/images/fairy.png',
+        img: './src/images/tin-man.png',
         appendIndex: "11"
     },
     {
         name: "tin man 3",
-        img: './src/images/fairy.png',
+        img: './src/images/tin-man.png',
         appendIndex: "12"
     },
     {
         name: "tin man 4",
-        img: './src/images/fairy.png',
+        img: './src/images/tin-man.png',
         appendIndex: "13"
     },
     {
         name: "zahel",
-        img: './src/images/fairy.png',
+        img: './src/images/elf-ranger.png',
         appendIndex: "14"
     },
     {
         name: "ice sprite",
-        img: './src/images/fairy.png',
+        img: './src/images/ice-sprite.png',
         appendIndex: "15"
     },
     {
         name: "cinderhild pillar 1",
-        img: './src/images/fairy.png',
+        img: './src/images/cinderhild.png',
         appendIndex: "16"
     },
     {
         name: "cinderhild pillar 2",
-        img: './src/images/fairy.png',
+        img: './src/images/cinderhild.png',
         appendIndex: "17"
     },
     {
         name: "angel unicorn",
-        img: './src/images/fairy.png',
+        img: './src/images/angel-unicorn.png',
         appendIndex: "18"
     },
     {
         name: "angel pegasus",
-        img: './src/images/fairy.png',
+        img: './src/images/angel-pegasus.png',
         appendIndex: "19"
     }
 ]
